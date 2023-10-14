6c1122d4e0b7b2b8fd268c6b7083a4b12b9f08cf
Nikita <efimov.n99@gmail.com>
2023-10-14 11:20:50 +0300

    fix

--- backend/db/database.py
+++ backend/db/database.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-import db.config as config
+from db import config
 
 
 engine = create_engine(
