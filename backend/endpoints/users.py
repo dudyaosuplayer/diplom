6c5e4eff645e5fb34ff2dde43a44507638d95b16
dudyaosuplayer <sasch2010pavlov@mail.ru>
2023-10-14 22:19:45 +0400

    Fix N30

--- backend/endpoints/users.py
+++ backend/endpoints/users.py
@@ -4,12 +4,11 @@ from fastapi import APIRouter, Path, Query, HTTPException
 
 from backend.auth.auth import auth_dependencies
 from backend.db.database import db_dependencies
-from backend.db.queries.users import get_users, get_user_by_username
+from backend.db.queries import users as users_q
 
 from backend.models.models import Project
 from backend.utils.fastapi.tags import Tags
 from backend.utils.fastapi.schemas.user_schemas import User, UserCreate, UserDelete
-
 
 router = APIRouter(prefix='/users', tags=[Tags.users])
 
@@ -21,7 +20,7 @@ def get_users(db: db_dependencies,
               limit: Annotated[int | None, Query(title="Limit number of entries",
                                                  description="Limit number of entries (100 is optimal)")] = 100):
     try:
-        users = get_users(db, skip=skip, limit=limit)
+        users = users_q.get_users(db, skip=skip, limit=limit)
         return users
     except Exception as e:
         raise e
@@ -29,6 +28,7 @@ def get_users(db: db_dependencies,
 
 @router.get("/get_users/{project_id}", description='This method returns all users from project')
 def get_project_users(db: db_dependencies,
+                      credentials: auth_dependencies,
                       project_id: int = Path(..., description="The ID of the project")):
     try:
         project = db.query(Project).filter(Project.id == project_id).first()
@@ -58,7 +58,7 @@ def create_user(user: UserCreate, db: db_dependencies):
         HTTP 400: Email already registered.
         HTTP 400: Username already registered.
     """
-    db_user_username = get_user_by_username(db, username=user.nickname)
+    db_user_username = users_q.get_user_by_username(db, username=user.nickname)
     if db_user_username:
         raise HTTPException(status_code=400, detail="Username already registered")
     return create_user(db=db, user=user)
