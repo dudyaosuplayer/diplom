6796ec16a9e9a6f88055247dce71f2173c5d5cfa
Nikita <efimov.n99@gmail.com>
2023-10-15 11:46:14 +0300

    oops

--- backend/main.py
+++ backend/main.py
@@ -17,4 +17,4 @@ add_routers(app)
 
 # Run the FastAPI application
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=8080, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
